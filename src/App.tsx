f582faa1bfddf76cc217031902b95db0a061ad94
Frank <frankshangqiandu@gmail.com>
2024-05-06 16:56:56 +1000

    7.2 react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,16 @@
 import React from 'react';
 import Styles from './App.module.css';
-import {Header,Footer,Carousel,SideMenu,ProductCollection, BusinessPartners} from "./components";
-import {Row,Col, Typography} from "antd";
-import { productList1, productList2, productList3 } from "./mockups";
-import sideImage from "./assets/images/sider_2019_12-09.png";
-import sideImage2 from "./assets/images/sider_2019_02-04.png";
-import sideImage3 from "./assets/images/sider_2019_02-04-2.png";
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import { HomePage } from './pages';
 
 function App() {
   return (
     <div className={Styles.App}>
-      <Header/>
-      {/* content */}
-      <div className={Styles['page-content']}>
-        <Row style={{marginTop: 20}}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={<Typography.Title level={3} type="warning">Recommend</Typography.Title>}
-          sideImage={sideImage}
-          products={productList1}
-        />
-        <ProductCollection
-          title={<Typography.Title level={3} type="danger">New</Typography.Title>}
-          sideImage={sideImage2}
-          products={productList2}
-        />
-        <ProductCollection
-          title={<Typography.Title level={3} type="success">Domestic travel recommendation</Typography.Title>}
-          sideImage={sideImage3}
-          products={productList3}
-        />
-      </div>
-      <BusinessPartners/>
-      <Footer/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage/>}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
