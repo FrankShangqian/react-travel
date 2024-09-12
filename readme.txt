65ecbee3a8620ff482df9277658ab35f7e0149d3
FrankShangqian <frankshangqiandu@gmail.com>
2024-09-12 21:46:42 +1000

    9.2 AJAX异步获取推荐数据

--- readme.txt
+++ readme.txt
@@ -2,3 +2,4 @@ npm install antd@4.20.4
 git push reproject main
 ssh -i "C:\Users\ASUS\.ssh\ssh-key-2024-04-18.key" ubuntu@158.179.21.14
 git commit --allow-empty -m '8.1 redux'
+icode: A43B6782E66655C9
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import 'antd/dist/antd.min.css';
 import "./i18n/configs";
 import {Provider} from "react-redux"
 import store from "./redux/store"
+import axios from "axios"
+
+axios.defaults.headers['x-icode']='A43B6782E66655C9';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
